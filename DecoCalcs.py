c1961380908492e197e055554644f136cfdcb3a9
Akshay Kashyap <the.akshay.kashyap@gmail.com>
2023-09-01 07:52:55 -0400

    Moved dive class to DecoCalcs

--- DecoCalcs.py
+++ DecoCalcs.py
@@ -16,6 +16,9 @@
 import sys
 import math
 
+import pprint
+import re
+
 # ZH-L12 from "Decompression"
 #
 ZHL12N = {
@@ -265,5 +268,107 @@ def buhlmann( Pn, an, bn, Phe = 0, ahe = 0, bhe = 0, gf = 1 ) :
     rc = num / den
     return round( rc, 4 )
 
-# eof
 #
+#
+class dive( object ) :
+    
+    # ctor
+    #
+    def __init__( self, verbose = False ) :
+
+        self._verbose = bool( verbose )
+        self._timepat = re.compile( r"(?:(\d{1,2}):)?(\d{1,2}):(\d{1,2})" )
+        
+# air, sea level, USN RQ. S is surface pressure (const), P is current pressure (var)
+        self._T = 0
+        self._S = 1.0
+        self._P = self._S
+        self._Q = 0.79
+        self._RQ = 0.9
+        self._GFHi = 0.85
+        self._TCs = []
+
+# starting Pt (same for all TCs)
+        sp = palv( Pamb = self._P, Q = self._Q, RQ = self._RQ )
+
+# use ZH-L16Cb (skip over 4-minute TC)
+        for tc in ZHL16N.keys() :
+            if tc == 1 : continue
+            self._TCs.append( { 
+                "t" : ZHL16N[tc]["t"], 
+                "a" : ZHL16N[tc]["a"]["C"],
+                "b" : ZHL16N[tc]["b"],
+                "P" : sp
+            } )
+
+# init. ceiling
+        for i in range( len( self._TCs ) ) :
+            self._TCs[i]["C"] = buhlmann( Pn = self._TCs[i]["P"],
+                                         an = self._TCs[i]["a"],
+                                         bn = self._TCs[i]["b"],
+                                         gf = self._GFHi )
+            
+        if self._verbose :
+            pprint.pprint( self._TCs )
+            
+    # helpers for plotting
+    # (could actually do this in a more "pythonic" way but this is more obvious)
+    #
+    @property
+    def compartments( self ) :
+        rc = []
+        for i in range( len( self._TCs ) ) :
+            rc.append( self._TCs[i]["t"] )
+        return rc
+    
+    @property
+    def loadings( self ) :
+        rc = []
+        for i in range( len( self._TCs ) ) :
+            rc.append( self._TCs[i]["P"] )
+        return rc
+
+    # helper function: takes human-readable time string like "1:30" and returns minutes: 1.5
+    #
+    def _time( self, t = "0:0" ) :
+        if t is None : return 0
+        m = self._timepat.search( str( t ).strip() )
+        if not m : raise Exception( "Invalid time string %s" % (t,) )
+        rc = 0.0
+        if m.group( 1 ) is not None :
+            rc = float( m.group( 1 ) ) * 60.0
+        rc += float( m.group( 2 ) )
+        rc += float( m.group( 3 ) ) / 60.0
+        return round( rc, 1 )
+
+    # newdepth is new depth in 0.1 bar
+    # timestr is time as [hours:]minutes:seconds string. *it is the total elapsed* time
+    #
+    def segment( self, newdepth = 0.0, newtimestr = "1:0" ) :
+        assert float( newdepth ) >= 0.0
+        if float( newdepth ) == 0.0 :
+            newP = self._P
+        else :
+            newP = round( self._S + float( newdepth ) / 10, 1 )
+        t = self._time( newtimestr ) - self._T
+    
+        for i in range( len( self._TCs ) ) :
+            p = schreiner( Pi = self._TCs[i]["P"], 
+                          Palv = palv( Pamb = self._P, Q = self._Q, RQ = self._RQ ), 
+                          t = t, 
+                          R = arr( d0 = self._P, dt = newP, t = t, Q = self._Q ),
+                          k = kay( Th = self._TCs[i]["t"] ) )
+            self._TCs[i]["P"] = p
+            self._TCs[i]["C"] = buhlmann( Pn = self._TCs[i]["P"],
+                                         an = self._TCs[i]["a"],
+                                         bn = self._TCs[i]["b"],
+                                         gf = self._GFHi )
+
+        self._P = newP
+        self._T += t
+    
+        if self._verbose :
+            sys.stdout.write( "* At time %f, P %f:\n" % (self._T, self._P,) )
+            pprint.pprint( self._TCs )
+
+
